01296c3afb64baacd196d876f08e0df6796b0d95
Binh Do Quoc <binhdq@fpt.edu.vn>
2020-01-16 17:10:45 +0700

    update node

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ var http = require('http');
 
 http.createServer((request,response)=>{
     response.writeHead(200,{'content-type':'text/html'});
-    response.end('Hello world1233');
+    let message ='';
+    for (let index = 1; index < 10; index++) {
+        message += `Hello world ${index}`;
+    }
+    response.end(message);
+   
 }).listen(8080);
